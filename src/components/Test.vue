cef3994103f13789272763c09525f7a3a0d81691
Bowen Chen <bowenchen@BowendeMacBook-Pro.local>
2018-01-13 15:12:02 -0800

    learn vue

---
+++ src/components/Test.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    {{msg}}
+
+    <div v-if="type === 'A'">This is A</div>
+    <div v-else-if="type === 'B'">This is B</div>
+    <div v-else>else</div>
+
+    <div v-show="ok">show me</div>
+
+    <ul>
+      <li v-bind:key="item" v-for="(item, index) in items" v-if="item.iscomplete ===false">
+        {{ parentMessage }} - {{ index }} - {{ item.message }}
+      </li>
+    </ul>
+
+    <ul>
+      <li v-bind:key="item.id" v-for="(item, index) in objects">
+        <!-- better to bind with item.id -->
+        {{ parentMessage }} - {{ index }} - {{ item }}
+      </li>
+    </ul>
+
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      parentMessage: 'parenT',
+      msg: 'ohhhhh',
+      type: 'A',
+      ok: false,
+      items: [
+        {
+          message: '1',
+          iscomplete: true
+        },
+        {
+          message: '2',
+          iscomplete: false
+        }
+      ],
+      objects: {
+        firstname: 'bob',
+        secondname: 'jason',
+        age: 30
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style "scoped">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Chat from '@/components/Chat'
-
+import Test from '@/components/Test'
 Vue.use(Router)
 
 export default new Router({
@@ -14,6 +14,11 @@ export default new Router({
       path: '/chat',
       name: 'chat',
       component: Chat
+    },
+    {
+      path: '/Test',
+      name: 'test',
+      component: Test
     }
   ]
 })
