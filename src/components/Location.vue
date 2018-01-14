2cc4e7b83100bb68ae1c3b7a9b28986f2f0a4973
VertexC <bob2420083992@gmail.com>
2018-01-14 00:01:16 -0800

    find some way to draw the circle

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="map" style="width:100%;height:400px;"></div>
+    <div id='map' style='width:100%;height:400px;'></div>
   </div>
 </template>
 
@@ -15,7 +15,25 @@ export default {
     return {
       msg: 'This is ' + this.$route.name,
       mapInstance: null,
-      mapMarker: null
+      mapMarker: null,
+      citymap: {
+        chicago: {
+          center: { lat: 41.878, lng: -87.629 },
+          population: 2714856
+        },
+        newyork: {
+          center: { lat: 40.714, lng: -74.005 },
+          population: 8405837
+        },
+        losangeles: {
+          center: { lat: 34.052, lng: -118.243 },
+          population: 3857799
+        },
+        vancouver: {
+          center: { lat: 49.25, lng: -123.1 },
+          population: 603502
+        }
+      }
     }
   },
   computed: {
@@ -26,7 +44,17 @@ export default {
   methods: {
     ...mapMutations([
       'setLocation'
-    ])
+    ]),
+    getCircle: function (rvMaps, magnitude) {
+      return {
+        path: rvMaps.SymbolPath.CIRCLE,
+        fillColor: 'red',
+        fillOpacity: 0.2,
+        scale: Math.pow(2, magnitude) / 2,
+        strokeColor: 'white',
+        strokeWeight: 0.5
+      }
+    }
   },
   mounted: function () {
     let self = this
@@ -251,13 +279,27 @@ export default {
             })
           })
         }
+        for (var city in self.citymap) {
+          // Add the circle for this city to the map.
+          var cityCircle = new rvMaps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: self.mapInstance,
+            center: self.citymap[city].center,
+            radius: Math.sqrt(self.citymap[city].population) * 100
+          })
+          console.log(cityCircle)
+        }
       })
     })
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style "scoped">
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style 'scoped'>
 
 </style>
