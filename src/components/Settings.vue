09e3e0e44db6b811c3eb1100b7db8230ce37a287
VertexC <bob2420083992@gmail.com>
2018-01-13 17:10:31 -0800

    complete aboutusin setting

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -8,9 +8,25 @@
     <mt-cell title="Privacy" label="" is-link></mt-cell>
     <mt-cell title="Histroy" label="Your chat histroy" is-link></mt-cell>
     <mt-cell title="Help&Feedback" label="" is-link></mt-cell>
-    <mt-cell title="About" label="About our team" @click.native="toAbout" is-link></mt-cell>
-
-
+    <mt-cell title="About" label="About our team" v-on:click.native="toAbout" is-link></mt-cell>
+    <mt-popup v-model="aboutPopupVisible" position="right">
+      <div style="width:100vw;height:100vh">
+        <div class="team-information" style="margin-top:100px;">
+          <div class="container" style="display:flex;justify-content:center;">
+            <div style="display:blcok">
+              <div style="font-size:50px;">nwHacks</div>
+              <div>Patric</div>
+              <div>Alex</div>
+              <div>Jason</div>
+              <div>Bob</div>
+            </div>
+          </div>
+        </div>
+        <div class="container" style="display:flex;justify-content:center;margin-top:70px;margin-bottom:20px">
+          <mt-button type="default" v-on:click.native="toSetting">go back</mt-button>
+        </div>
+      </div>
+    </mt-popup>
   </div>
 </template>
 
@@ -19,12 +35,18 @@ export default {
   name: 'settings',
   data() {
     return {
-      msg: 'This is ' + this.$route.name
+      msg: 'This is ' + this.$route.name,
+      aboutPopupVisible: false
     }
   },
   methods: {
     toAbout: function () {
-      this.$router.push('')
+      // this.$router.push('')
+      // console.log('here')
+      this.aboutPopupVisible = true
+    },
+    toSetting: function () {
+      this.aboutPopupVisible = false
     }
   }
 }
@@ -32,5 +54,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style "scoped">
-
+.team-information {
+  margin-left: 20px;
+  margin-right: 500px;
+}
 </style>
